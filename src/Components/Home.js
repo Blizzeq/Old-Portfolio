04b94afd51024eba010bc5365e102afef08f4fb6
Blizzeq <kubamax4@gmail.com>
2022-09-14 23:38:46 +0200

    Solve problem with collapse icon color

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import {ThemeContext} from "../App";
 
 
 function Home(props) {
+
+
     return (
         <div className={"Home"}>
 
