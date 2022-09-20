9a9aafb17b838eb0fa9deea1769e2c824cb0e5bb
jakubkrasuski <kubamax4@gmail.com>
2022-09-20 20:57:07 +0200

    Added <Footer/> component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ import Typewriter from 'typewriter-effect';
 import Education from "./Education";
 import {useInView} from "framer-motion";
 import Knowledge from "./Knowledge";
+import Footer from "./Footer";
 
 
 function Home(props) {
@@ -14,10 +15,10 @@ function Home(props) {
 
     const [isInViewInfo, setIsInViewInfo] = useState('');
 
-    function checkInView(){
-        if(isInView){
+    function checkInView() {
+        if (isInView) {
             setIsInViewInfo('Appear');
-        }else{
+        } else {
             setIsInViewInfo('');
         }
     }
@@ -30,29 +31,30 @@ function Home(props) {
 
     return (
         <>
-        <div className={`Home-${themeMode}`}>
-            <div className={'Home-Content'}>
-                <div className={'Content'}>
-                    <p>👋 Hello, it's Jakub Krasuski.</p>
-                </div>
-                <div className={'Content'}>
-                    <Typewriter
-                        options={{
-                            strings: ['Junior React Developer', 'IT Student', 'Enthusiast of technologies'],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                </div>
-                <div className={'Content'}>
-                </div>
-                <div className={`Content ${isInViewInfo}`} ref={isInViewRef}>
-                    <p>Let me introduce myself</p>
+            <div className={`Home-${themeMode}`}>
+                <div className={'Home-Content'}>
+                    <div className={'Content'}>
+                        <p>👋 Hello, it's Jakub Krasuski.</p>
+                    </div>
+                    <div className={'Content'}>
+                        <Typewriter
+                            options={{
+                                strings: ['Junior React Developer', 'IT Student', 'Enthusiast of technologies'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </div>
+                    <div className={'Content'}>
+                    </div>
+                    <div className={`Content ${isInViewInfo}`} ref={isInViewRef}>
+                        <p>Let me introduce myself</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Education/>
-        <Knowledge/>
+            <Education/>
+            <Knowledge/>
+            <Footer/>
         </>
     );
 }
